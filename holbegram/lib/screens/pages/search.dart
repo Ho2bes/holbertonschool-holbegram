a825c26136319c0565ceeba4ae86c35c61e10622
Ho2bes <8696@holbertonstudents.com>
2025-07-24 09:17:13 +0200

    Refactor Search screen to use StatefulWidget; implement posts display with Firestore integration and MasonryGridView layout

--- holbegram/lib/screens/pages/search.dart
+++ holbegram/lib/screens/pages/search.dart
@@ -1,13 +1,60 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 
-class Search extends StatelessWidget {
-  const Search({super.key});
+class Search extends StatefulWidget {
+  const Search({Key? key}) : super(key: key);
 
   @override
+  State<Search> createState() => _SearchState();
+}
+
+class _SearchState extends State<Search> {
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Search Page'),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Search',
+          style: TextStyle(
+            fontFamily: 'Billabong',
+            fontSize: 32,
+            color: Colors.black,
+          ),
+        ),
+        backgroundColor: Colors.white,
+        centerTitle: true,
+        elevation: 0,
+      ),
+      body: StreamBuilder(
+        stream: FirebaseFirestore.instance.collection('posts').snapshots(),
+        builder: (context, AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          }
+
+          final posts = snapshot.data!.docs;
+
+          return Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: MasonryGridView.count(
+              crossAxisCount: 2,
+              mainAxisSpacing: 8,
+              crossAxisSpacing: 8,
+              itemCount: posts.length,
+              itemBuilder: (context, index) {
+                final post = posts[index].data();
+                return ClipRRect(
+                  borderRadius: BorderRadius.circular(12),
+                  child: Image.network(
+                    post['postUrl'],
+                    fit: BoxFit.cover,
+                  ),
+                );
+              },
+            ),
+          );
+        },
       ),
     );
   }
